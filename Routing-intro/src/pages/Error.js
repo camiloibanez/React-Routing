899f956719bb6dfe1d61c34dac9d8569ec1e32c6
Camilo Ibanez <camiloibanez8@gmail.com>
2023-03-27 11:49:02 -0500

    React Router

---
+++ Routing-intro/src/pages/Error.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function ErrorPage() {
+    return (
+        <Fragment>
+            <main>
+                <h1>An error occurred!</h1>
+                <p>Could not find this page!</p>
+            </main>
+        </Fragment>
+    );
+}
+
+export default ErrorPage;
